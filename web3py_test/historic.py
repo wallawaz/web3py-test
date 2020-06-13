552c68eb706996896cf783b55c4eaf43ccaa5d9c
Ben Wallad <bwallad@gmail.com>
2020-06-13 17:44:51 +0000

    update historic params

--- web3py_test/historic.py
+++ web3py_test/historic.py
@@ -7,11 +7,14 @@ from .logger import logger
 class HistoricExchanges:
     def __init__(self, factory_contract, from_block):
         self.factory_contract = Web3.toChecksumAddress(factory_contract)
-        self.factory_contract = factory_contract
+
         self.from_block = from_block
+        if from_block != "latest":
+            self.from_block = hex(int(from_block))
 
     def run(self):
         contract = w3.eth.contract(self.factory_contract, abi=NEW_EXCHANGE_ABI)
+        logger.info(f"from block: {self.from_block}")
 
         event_filter = contract.events.NewExchange.createFilter(fromBlock=self.from_block)
         for event in event_filter.get_all_entries():
