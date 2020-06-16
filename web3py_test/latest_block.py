6cf83ac12a36725d240bca9eafd18a2cec58611b
Ben Wallad <bwallad@gmail.com>
2020-06-16 19:26:00 +0000

    refactor latest_block script

--- web3py_test/latest_block.py
+++ web3py_test/latest_block.py
@@ -1,15 +1,43 @@
+import csv
+from datetime import datetime
 import os
 import time
 
 WEB3_PROVIDER_URI = os.getenv("WEB3_PROVIDER_URI")
 API_KEY = os.getenv("ETHERSCAN_KEY")
 
+HEADERS = ["ts", "source", "blocknumber", "blockhash"]
+
+def get_filename():
+    fn = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    if API_KEY is not None:
+        return {"filename": f"{fn}_etherscan.csv", "provider_type": "etherscan"}
+
+    provider_formats = {"file": "local_ipc", "ws": "infura_ws"}
+    provider_type = WEB3_PROVIDER_URI.split(":")[0]
+    return {
+       "filename": f"{fn}_{provider_type}.csv",
+       "provider_type": provider_type,
+    }
+
 def main_web3():
     from web3.auto import w3
-    print(f"WEB3: {WEB3_PROVIDER_URI}")
-    for i in range(1000):
-        latest_block = w3.eth.getBlock("latest").get("transactionsRoot")
-        print(f"{time.time_ns()}: tx: {latest_block.hex()}")
+    file_name_dict = get_filename()
+
+    rows = []
+    with open(file_name_dict["filename"], "w") as of:
+        csvw = csv.DictWriter(of, fieldnames=HEADERS)
+        csvw.writeheader()
+        for i in range(1000):
+            latest_block = w3.eth.getBlock("latest")
+            rows.append({
+              "ts":time.time_ns(),
+              "source":file_name_dict["provider_type"],
+              "blocknumber":latest_block.get("number"),
+              "blockhash":latest_block.get("transactionsRoot").hex(),
+            })
+        csvw.writerows(rows)
+
 
 
 # The Etherscan Ethereum Developer APIs are provided as a community service and
@@ -17,19 +45,26 @@ def main_web3():
 # both GET/POST requests and there is a rate limit of 5 calls per sec/IP.
 def main_etherscan():
     import etherscan
-    print("ETHERSCAN")
+    file_name_dict = get_filename()
     es = etherscan.Client(
     api_key=API_KEY,
     cache_expire_after=5,
     )
-    calls = 0
-    for i in range(1000):
-        block = es.get_block_number()
-        print(f"{time.time_ns()}: {block}")
-        calls += 1
-        if calls == 5:
-            time.sleep(5)
-            calls = 0
+    rows = []
+    with open(file_name_dict["filename"], "w") as of:
+        csvw = csv.DictWriter(of, fieldnames=HEADERS)
+        csvw.writeheader()
+        for i in range(1000):
+            blocknumber = es.get_block_number()
+            blockhash = es.get_block_by_number(blocknumber).get("transactionsRoot")
+            rows.append({
+              "ts":time.time_ns(),
+              "source":file_name_dict["provider_type"],
+              "blocknumber":blocknumber,
+              "blockhash":blockhash,
+            })
+            time.sleep(.215)
+        csvw.writerows(rows)
 
 def main():
     if WEB3_PROVIDER_URI is not None:
@@ -41,5 +76,4 @@ def main():
 
 if __name__ == "__main__":
     main()
-
 
